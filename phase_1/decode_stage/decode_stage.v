9614ecd295149ad8f566bbeea2c985fe78002e2e
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-11-07 21:15:55 +0200

    ..

---
+++ phase_1/decode_stage/decode_stage.v
@@ -0,0 +1,43 @@
+module decode_stage(
+    input clk,
+    input rst
+);
+
+    ControlUnit
+    ControlUnit_dut (
+        .inst (inst ),
+        .ALUOp (ALUOp ),
+        .ALUtoReg (ALUtoReg ),
+        .RegWrite (RegWrite ),
+        .MemRead (MemRead ),
+        .MemWrite  ( MemWrite)
+    );
+
+
+    reg_file
+    #(
+    .WIDTH(16 ),
+    .N_REGS (
+    8 )
+    )
+    reg_file_dut (
+        .clk (clk ),
+        .rst (rst ),
+        .RegWrite (RegWrite ),
+        .write_address (write_address ),
+        .write_data (write_data ),
+        .read_address1 (read_address1 ),
+        .read_address2 (read_address2 ),
+        .read_data1 (read_data1 ),
+        .read_data2  ( read_data2)
+    );
+
+
+    sign_extend
+    sign_extend_dut (
+        .in (in ),
+        .out  ( out)
+    );
+
+
+endmodule
