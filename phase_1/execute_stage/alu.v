f5faaf843a1c58a021b8bf61143dce89f2e2bbc8
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-11-10 08:54:46 +0200

    renamed the reg

--- phase_1/execute_stage/alu.v
+++ phase_1/execute_stage/alu.v
@@ -5,17 +5,26 @@ module alu(
     output reg [15:0] result
 );
     // we stick to dest being the first operand
+    wire reg [15:0] result_s;
+
+    var_reg  #(.size(16))
+    pip_EX_MEM (
+        .clk (clk ),
+        .D (result_s ),
+        .Q  ( result)
+    );
+
     always @* begin
         case (AlUmode)
             //Add
-            2'b00 : result = Op1 + Op2;
+            2'b00 : result_s = Op1 + Op2;
             //NOT
-            2'b01 : result =  ~Op1;
+            2'b01 : result_s =  ~Op1;
             //PASS dest Register
-            2'b10 : result = Op1;
+            2'b10 : result_s = Op1;
             //NOP
-            2'b11 : result = 16'b0;
-            default : result = 16'b0;
+            2'b11 : result_s = 16'b0;
+            default : result_s = 16'b0;
         endcase
     end
 
---
+++ phase_1/var_reg.v
@@ -0,0 +1,10 @@
+module var_reg #(parameter size = 8)(D,clk,Q);
+    input [size-1:0] D;
+    input clk;
+    output reg [size-1:0] Q;
+
+    always @(posedge clk)
+    begin
+        Q <= D;
+    end
+endmodule 
