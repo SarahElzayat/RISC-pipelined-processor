422af43f08f8940c355b4e17eb0a278f861ac7d3
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-12-03 13:10:25 +0200

    Almost working

--- phase_1/processor_tb.v
+++ phase_1/processor_tb.v
@@ -6,6 +6,7 @@ module processor_tb;
     reg clk = 0;
     reg rst = 0;
     wire [2:0] conditionCodeRegister;
+    reg expected = 1'bx;
     reg [16 -1 :0] R [0:8 -1];
 
     processor
@@ -25,18 +26,41 @@ module processor_tb;
 
     initial begin
         begin
-            $monitor("Time = %t | R1 = %b | R2 = %b | Memory at R1 = %b | Carry = %b | Negative = %b | Zero = %b", $time, 
-			R[1], R[2], processor_dut.memory_stage_dut.data_memory[2045], conditionCodeRegister[2], conditionCodeRegister[1], conditionCodeRegister[0]);
+            $monitor("Time = %t | R1 = %b | R2 = %b | Memory at R1 = %b | Carry = %b | Negative = %b | Zero = %b | Expected = %b", $time, 
+			R[1], R[2], processor_dut.memory_stage_dut.data_memory[2045], conditionCodeRegister[2], conditionCodeRegister[1], conditionCodeRegister[0], expected);
             #CLK_PERIOD rst = 1;
             #CLK_PERIOD rst = 0;
 
-            #(CLK_PERIOD* 25)
+            #(CLK_PERIOD * 5)
+            expected = 1'bx;
+            if (conditionCodeRegister == 3'b001 && R[1] == 1'd0) expected = 1;
 
-            $stop;
+            #(CLK_PERIOD * 5)
+            expected = 1'bx;
+            if (conditionCodeRegister == 3'b001 && R[2] == 16'b0000000000000010) expected = 1;
+
+            #(CLK_PERIOD * 5)
+            expected = 1'bx;
+            if (conditionCodeRegister == 3'b001) expected = 1;
+
+            #(CLK_PERIOD * 5)
+            expected = 1'bx;
+            if (conditionCodeRegister == 3'b000 && R[1] == 16'b0000000000000010) expected = 1;
+
+            #(CLK_PERIOD * 5)
+            expected = 1'bx;
+            if (conditionCodeRegister == 3'b010 && R[1] == 16'b1111111111111101) expected = 1;
+
+            #(CLK_PERIOD * 5)
+            expected = 1'bx;
+            if (conditionCodeRegister == 3'b010 && processor_dut.memory_stage_dut.data_memory[2045] == 16'b0000000000000010) expected = 1;
+
+            //#(CLK_PERIOD)
+            //$finish;
         end
     end
 
     always
-    #5  clk = ! clk ;
+    #(CLK_PERIOD / 2)  clk = ! clk ;
 
 endmodule
