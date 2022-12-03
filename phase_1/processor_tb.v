55c4d1258255ad366558ce16ab0fafb8d278734a
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-12-03 16:48:27 +0200

    TB

--- phase_1/processor_tb.v
+++ phase_1/processor_tb.v
@@ -17,6 +17,7 @@ module processor_tb;
     );
 
     integer i;	
+    integer successCount = 0;
 
     always @(*) begin
         for (i = 0; i < 8; i = i + 1) begin
@@ -32,41 +33,49 @@ module processor_tb;
             #CLK_PERIOD rst = 0;
             #CLK_PERIOD;
             #(CLK_PERIOD * 5)
-            expected = 1'bx;
+            expected = 1'b0;
             if (conditionCodeRegister == 3'b001 && R[1] == 1'd0) expected = 1; // LDM R1, 0
             $display("Time = %t | R1 = %b | R2 = %b | Memory at R1 = %b | Carry = %b | Negative = %b | Zero = %b | Expected = %b", $time, 
 			R[1], R[2], processor_dut.memory_stage_dut.data_memory[2045], conditionCodeRegister[2], conditionCodeRegister[1], conditionCodeRegister[0], expected);
-           
+            successCount = successCount + expected;
             #(CLK_PERIOD * 5)
-            expected = 1'bx;
+            expected = 1'b0;
             if (conditionCodeRegister == 3'b001 && R[2] == 16'b0000000000000010) expected = 1; // LDM R2, 2
             $display("Time = %t | R1 = %b | R2 = %b | Memory at R1 = %b | Carry = %b | Negative = %b | Zero = %b | Expected = %b", $time, 
 			R[1], R[2], processor_dut.memory_stage_dut.data_memory[2045], conditionCodeRegister[2], conditionCodeRegister[1], conditionCodeRegister[0], expected);
+            successCount = successCount + expected;
            
             #(CLK_PERIOD * 5)
-            expected = 1'bx;
+            expected = 1'b0;
             if (conditionCodeRegister == 3'b001) expected = 1; // NOP
             $display("Time = %t | R1 = %b | R2 = %b | Memory at R1 = %b | Carry = %b | Negative = %b | Zero = %b | Expected = %b", $time, 
 			R[1], R[2], processor_dut.memory_stage_dut.data_memory[2045], conditionCodeRegister[2], conditionCodeRegister[1], conditionCodeRegister[0], expected);
+            successCount = successCount + expected;
            
             #(CLK_PERIOD * 5)
-            expected = 1'bx;
+            expected = 1'b0;
             if (conditionCodeRegister == 3'b000 && R[1] == 16'b0000000000000010) expected = 1; // ADD R1, R2
             $display("Time = %t | R1 = %b | R2 = %b | Memory at R1 = %b | Carry = %b | Negative = %b | Zero = %b | Expected = %b", $time, 
 			R[1], R[2], processor_dut.memory_stage_dut.data_memory[2045], conditionCodeRegister[2], conditionCodeRegister[1], conditionCodeRegister[0], expected);
+            successCount = successCount + expected;
            
             #(CLK_PERIOD * 5)
-            expected = 1'bx;
+            expected = 1'b0;
             if (conditionCodeRegister == 3'b010 && R[1] == 16'b1111111111111101) expected = 1; // NOT R1
             $display("Time = %t | R1 = %b | R2 = %b | Memory at R1 = %b | Carry = %b | Negative = %b | Zero = %b | Expected = %b", $time, 
 			R[1], R[2], processor_dut.memory_stage_dut.data_memory[2045], conditionCodeRegister[2], conditionCodeRegister[1], conditionCodeRegister[0], expected);
+            successCount = successCount + expected;
            
             #(CLK_PERIOD * 5)
-            expected = 1'bx;
+            expected = 1'b0;
             if (conditionCodeRegister == 3'b010 && processor_dut.memory_stage_dut.data_memory[2045] == 16'b0000000000000010) expected = 1; // STD R2, R1
             $display("Time = %t | R1 = %b | R2 = %b | Memory at R1 = %b | Carry = %b | Negative = %b | Zero = %b | Expected = %b", $time, 
 			R[1], R[2], processor_dut.memory_stage_dut.data_memory[2045], conditionCodeRegister[2], conditionCodeRegister[1], conditionCodeRegister[0], expected);
+            successCount = successCount + expected;
+
+            $display("number of instructions passed = %d", successCount); 
            
+            
             #(CLK_PERIOD)
             $stop;
         end
