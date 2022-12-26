93ccb30ed0273a17c44ac5347e7e3f0a9fde6bdf
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-12-26 20:24:12 +0200

    Execute stage with new inputs & buffers

--- phase_2/execute_stage/execute_stage.v
+++ phase_2/execute_stage/execute_stage.v
@@ -1,134 +1,189 @@
 module execute_stage (
-    input clk, // Clock
-    input reset, // The reset signal resets the PC to 2^5 (First address of the instructions memory)
-    input [15:0] Op1, // First operand
-    input [15:0] Op2, // Second operand
-    input [1:0] AlUmode, // ALU Operation
-    output [15:0] result_r, // ALU Result
-    output [2:0] conditionCodeRegister_r, // Carry, negative, zero
-    input [1:0] carrySelect, // Set, reset, ALU
+    input clk,
+    input reset,
 
-    // inputs to just pass to the next stage
-    input RegWrite,
-    output RegWrite_r,
+    // Inputs to ALU
+    input [1:0] carry_sel,
+    input [1:0] alu_src2_select, // Select src
+    input [15:0] read_data1, // Rdest
+    input [15:0] read_data2, // Rsrc
+    input [1:0] alu_src1_select, // Select dest
+    input [4:0] shamt,
+    input [3:0] ALU_Op,
+    input [15:0] write_back_data,
+    input [15:0] reg_data1_from_mem,
+    input [15:0] reg_data2_from_mem,
+    input [2:0] jump_selector,
+    input flag_regsel,
+    input flagreg_enable,
+    input [2:0] conditions_from_memory_pop,
 
-    input [2:0] reg_write_address_from_decode,
-    output [2:0] reg_write_address_to_memory,
+    output [15:0] result_out, // ALU Result
+    output [31:0] new_PC_out, // ALU Result
+    output [2:0] flag_register_out, // Carry, negative, zero
 
-    input [15:0] sign_extend_from_decode,
-    output [15:0] sign_extend_to_memory,
+    // inputs to just pass
+    input [31:0] PC,
+    output [31:0] PC_out,
 
-    input write_back_select_from_decode,
-    output write_back_select_to_memory,
+    input [15:0] LDM_value,
+    output [15:0] LDM_value_out,
 
-    input [15:0] reg_file_read_data1_from_decode,
-    input [15:0] reg_file_read_data2_from_decode,
+    input mem_pop,
+    output mem_pop_out,
+    
+    input mem_push,
+    output mem_push_out,
 
-    output [15:0] reg_file_read_data1_to_mem,
-    output [15:0] reg_file_read_data2_to_mem,
+    input reg_write,
+    output reg_write_out,
 
-    input memRead_from_decode,
-    output memRead_to_mem,
+    input [1:0] wb_sel,
+    output [1:0] wb_sel_out,
 
-    input memWrite_from_decode,
-    output memWrite_to_mem
+    input mem_read,
+    output mem_read_out,
+    
+    input mem_write,
+    output mem_write_out,
+
+    input pc_enable,
+    output pc_enable_out,
+
+    input [1:0] memory_address_select,
+    output [1:0] memory_address_select_out,
+
+    input [1:0] memory_write_src_select,
+    output [1:0] memory_write_src_select_out
 );
 
-    // ALU Result
-    wire [15:0] result;
-    var_reg  #(.size(16))
-    pip_EX_MEM (
-        .clk (clk),
-        .rst(reset),
-        .D (result),
-        .Q (result_r)
-    );
+wire [15:0] result;
+wire [31:0] new_PC;
+wire [2:0] flag_register;
+wire branch_result = 0;
 
-    // Condition Code Register
-    wire [2:0] conditionCodeRegister;
-    var_reg  #(.size(3))
-    pip_EX_MEMM (
-        .clk (clk && AlUmode!=2'b11),
-        .rst(reset),
-        .D (conditionCodeRegister),
-        .Q (conditionCodeRegister_r)
-    );
+// TODO 
+// Insert Forwarding Unit here
 
-    // RegWrite signal
-    var_reg  #(.size(1))
-    pip_EX_MEM_regs (
-        .clk (clk),
-        .rst(reset),
-        .D (RegWrite),
-        .Q (RegWrite_r)
-    );
+branch_controller branching (
+    .jump_selector (jump_selector),
+    .condition_signals (flag_register),
+    .result (branch_result)
+);
 
-    // RegWrite address
-    var_reg  #(.size(3))
-    pip_EX_MEM_regs2 (
-        .clk (clk),
-        .rst(reset),
-        .D (reg_write_address_from_decode),
-        .Q (reg_write_address_to_memory)
-    );
+assign new_PC =
+(branch_result == 0) ? PC :
+(branch_result == 1) ? {{16{read_data1[31]}}, read_data1} : PC;
 
-    // Signed-extended bits
-    var_reg  #(.size(16))
-    pip_EX_MEM_regs3 (
-        .clk (clk),
-        .rst(reset),
-        .D (sign_extend_from_decode),
-        .Q (sign_extend_to_memory)
-    );
+// BUFFERS
 
-    // Write-back signal
-    var_reg  #(.size(1))
-    pip_EX_MEM_regs4 (
-        .clk (clk),
-        .rst(reset),
-        .D (write_back_select_from_decode),
-        .Q (write_back_select_to_memory)
-    );
+var_reg #(.size(16))
+buffer1 (
+    .clk (clk),
+    .rst(reset),
+    .D (result),
+    .Q (result_out)
+);
 
-    // Read_Data 1 & 2
-    var_reg  #(.size(16*2))
-    pip_EX_MEM_regs45 (
-        .clk (clk ),
-        .rst(reset),
-        .D ({
-        reg_file_read_data1_from_decode,
-        reg_file_read_data2_from_decode
-        }),
-        .Q ({
-        reg_file_read_data1_to_mem,
-        reg_file_read_data2_to_mem
-        })
-    );
+var_reg #(.size(3))
+buffer2 (
+    .clk (clk && ALU_Op!=4'b1111),
+    .rst(reset),
+    .D (flag_register),
+    .Q (flag_register_out)
+);
 
-    // memRead & memWrite signals
-    var_reg  #(.size(2))
-    pip_EX_MEM_regs5 (
-        .clk (clk),
-        .rst (reset),
-        .D ({memRead_from_decode,
-        memWrite_from_decode
-        }),
-        .Q ({
-        memRead_to_mem,
-        memWrite_to_mem
-        })
-    );
+var_reg #(.size(2))
+buffer3 (
+    .clk (clk),
+    .rst(reset),
+    .D ({reg_write, pc_enable}),
+    .Q ({reg_write_out, pc_enable_out})
+);
 
-    // ALU
-    alu
-    alu_dut (
-        .Op1 (Op1),
-        .Op2 (Op2),
-        .AlUmode (AlUmode),
-        .carrySelect (carrySelect),
-        .conditionCodeRegister (conditionCodeRegister),
-        .result (result)
-    );
+var_reg #(.size(4))
+buffer4 (
+    .clk (clk),
+    .rst(reset),
+    .D ({memory_address_select, memory_write_src_select}),
+    .Q ({memory_address_select_out, memory_write_src_select_out})
+);
+
+var_reg #(.size(2))
+buffer5 (
+    .clk (clk),
+    .rst(reset),
+    .D (wb_sel),
+    .Q (wb_sel_out)
+);
+
+var_reg #(.size(32))
+buffer6 (
+    .clk (clk),
+    .rst(reset),
+    .D (PC),
+    .Q (PC_out)
+);
+
+var_reg #(.size(32))
+buffer6 (
+    .clk (clk),
+    .rst(reset),
+    .D (new_PC),
+    .Q (new_PC_out)
+);
+
+var_reg #(.size(16))
+buffer7 (
+    .clk (clk),
+    .rst(reset),
+    .D (LDM_value),
+    .Q (LDM_value_out)
+);
+
+var_reg  #(.size(32))
+buffer8 (
+    .clk (clk),
+    .rst(reset),
+    .D ({read_data1, read_data2}),
+    .Q ({read_data1_out, read_data2_out})
+);
+
+var_reg  #(.size(2))
+buffer9 (
+    .clk (clk),
+    .rst (reset),
+    .D ({mem_pop, mem_push}),
+    .Q ({mem_pop_out, mem_push_out})
+);
+
+var_reg  #(.size(2))
+buffer10 (
+    .clk (clk),
+    .rst (reset),
+    .D ({mem_read, mem_write}),
+    .Q ({mem_read_out, mem_write_out})
+);
+
+// ALU
+alu
+alu_dut (
+    .carry_sel (carry_sel),
+    .alu_src2_select (alu_src2_select),
+    .read_data1 (read_data1),
+    .read_data2 (read_data2),
+    .alu_src1_select (alu_src1_select),
+    .shamt (shamt),
+    .ALU_Op (ALU_Op),
+    .write_back_data (write_back_data),
+    .reg_data1_from_mem (reg_data1_from_mem),
+    .reg_data2_from_mem (reg_data2_from_mem),
+    .flag_regsel (flag_regsel),
+    .flagreg_enable (flagreg_enable),
+    .conditions_from_memory_pop (conditions_from_memory_pop),
+    .flag_register (flag_register),
+    .result (result)
+);
 
 endmodule
 
